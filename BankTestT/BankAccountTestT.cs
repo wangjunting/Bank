cca95d4460fe3e29bd6eda3ea3335469292f4116
wangjunting <wangjunting@juxinli.com>
2017-09-04 18:46:27 +0800

    CSharp demo

---
+++ BankTestT/BankAccountTestT.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BankTestT
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class BankAccountTestsT
+    {
+
+        // unit test code 
+        [TestMethod]
+        public void Credit_WithValidAmount()
+        {
+            // arrange
+            double beginningBalance = 10.00;
+            double Amount = 4.55;
+            double expected = 14.55;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            // act
+            account.Credit(Amount);
+            // assert 
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not credited correctly");
+        }
+       
+        [TestMethod]
+        public void Credit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange
+            double beginningBalance = 11.99;
+            double Amount = -10.0;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            // act
+            try
+            {
+                account.Credit(Amount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // assert
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountLessThanZeroMessage);
+                return;
+            }
+            Assert.Fail("No exception was thrown.");
+        }
+    }
+}
